aa3be764906941c462008bd020bac65a116b8be9
bryannbarbosa <bryannbarbosa@gmail.com>
2017-07-13 02:43:54 -0300

    update application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ angular
   ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $locationProvider.html5Mode(true);
-    var accessToken = '61d9c13fd5d8d884e64a856a9d1c477f088079a4205f79e500da6c0bd10f4821';
+    var accessToken = 'YOUR_CLIENT_ACCESS_TOKEN';
     $httpProvider.defaults.headers.common.Authorization = 'Bearer ' + accessToken;
     $routeProvider
       .when('/', {
